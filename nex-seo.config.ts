517fcd5273201cdd070dd28c43734d39913fa1ed
Ahmed Azzam <ahmed195138@feng.bu.edu.eg>
2022-11-24 13:53:44 +0200

    next-seo config

--- nex-seo.config.ts
+++ nex-seo.config.ts
@@ -9,6 +9,24 @@ export const NEXT_SEO_DEFAULT: DefaultSeoProps = {
         locale: 'en_IE',
         url: "CHANGE MEE",
         siteName: 'Zagy blog',
+        images: [
+            {
+                url: 'https://www.example.ie/og-image-01.jpg',
+                width: 800,
+                height: 600,
+                alt: 'Og Image Alt',
+                type: 'image/jpeg',
+            },
+            {
+                url: 'https://www.example.ie/og-image-02.jpg',
+                width: 900,
+                height: 800,
+                alt: 'Og Image Alt Second',
+                type: 'image/jpeg',
+            },
+            { url: 'https://www.example.ie/og-image-03.jpg' },
+            { url: 'https://www.example.ie/og-image-04.jpg' },
+        ],
     },
     twitter: {
         handle: '@handle',
