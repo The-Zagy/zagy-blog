ecbfed2eb41c316e65feed9961597f6a4b3a680f
Ahmed Azzam <ahmed195138@feng.bu.edu.eg>
2022-11-29 16:52:48 +0200

    basic revalidation github action

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -5,30 +5,37 @@ import { NUMBER_OF_POSTS_IN_A_PAGE } from "../../env/constants";
 // TODO DADDY PLEASE USE JWT
 const verifyToken = async (token: string): Promise<boolean> => {
     return true;
-} 
+}
 const handler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-    if (!req.query.secret ||!(typeof req.query.secret === 'string')|| !await verifyToken(req.query.secret)) {
-        return res.status(401).json({message:"Invalid Token | Not authorized"});
-    }
-    if(!req.query.slug){
-        return res.status(400).json({messege:"A slug must be supplied"})
+    if (!req.headers.authorization || !(typeof req.headers.authorization === 'string') || !await verifyToken(req.headers.authorization)) {
+        return res.status(401).json({ message: "Invalid Token | Not authorized" });
     }
 
-    try {
-        await GitHubFilesCache.updatePosts();
-        const githubFiles = await GitHubFilesCache.getPosts();
-        const fileIndex = githubFiles.findIndex((i=>i.meta.slug===req.query.slug));
-        const isNewlyCreated = fileIndex===githubFiles.length-1?true:false;
-        if(!isNewlyCreated){
-            res.revalidate(`/blog/post/${githubFiles[fileIndex]?.meta.slug}`);
-        }
-        //update the posts page
-        const pageNumber = Math.floor(fileIndex/NUMBER_OF_POSTS_IN_A_PAGE+1)+1;
-         res.revalidate(`/blog/${pageNumber}`);   
-         return res.status(200).json({messege:`update post ${githubFiles[fileIndex]?.meta.slug} and page number ${pageNumber}`});
-    } catch (e) {
-        console.log(e)
-        res.status(500).json({message: 'error happend unknown'});
+    if (!Array.isArray(req.body.slugs)) {
+        return res.status(400).json({ messege: "an array of changed or added files must be supplied" })
     }
-} 
+    const homePagesToBeRevalidated = new Set<number>;
+    await GitHubFilesCache.updatePosts(/* postToBeUpdated */);
+    for (const slug of req.body.slugs) {
+        try {
+            // await GitHubFilesCache.updatePosts(/* postToBeUpdated */);
+            const githubFiles = await GitHubFilesCache.getPosts();
+            const fileIndex = githubFiles.findIndex((i => i.meta.slug === slug));
+            const isNewlyCreated = fileIndex === githubFiles.length - 1 ? true : false;
+            if (!isNewlyCreated) {
+                res.revalidate(`/blog/post/${githubFiles[fileIndex]?.meta.slug}`);
+            }
+            //update the posts page
+            const pageNumber = Math.floor(fileIndex / NUMBER_OF_POSTS_IN_A_PAGE + 1) + 1;
+            homePagesToBeRevalidated.add(pageNumber);
+            return res.status(200).json({ messege: `update post ${githubFiles[fileIndex]?.meta.slug} and page number ${pageNumber}` });
+        } catch (e) {
+            console.log(e)
+            res.status(500).json({ message: 'error happend unknown' });
+        }
+        for (const page of homePagesToBeRevalidated) {
+            res.revalidate(`/blog/${page}`);
+        }
+    }
+}
 export default handler;
