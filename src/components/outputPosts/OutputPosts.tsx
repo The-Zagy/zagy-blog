6c1a3452eca29cdb4811c8563d8db7332bdab657
Ahmed Azzam <ahmed195138@feng.bu.edu.eg>
2022-12-02 10:39:46 +0200

    temporarily commented out search func

--- src/components/outputPosts/OutputPosts.tsx
+++ src/components/outputPosts/OutputPosts.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { Post } from '../../utils/mdx';
 import { PostCard } from '../../pages/blog/[pageNumber]';
 import { useContext } from 'react';
-import { OutputPostsContext } from '../../pages';
+// import { OutputPostsContext } from '../../pages';
 const OutputPosts: React.FC = () => {
-    const [posts, _] = useContext(OutputPostsContext)
+    // const [posts, _] = useContext(OutputPostsContext)
     return (
         <div className='flex flex-wrap'>
-        {posts.length>0 ? posts.map((post) => {
+            {/* {posts.length>0 ? posts.map((post) => {
             return (<PostCard {...(post.meta)} data={post.code} big={false} key={post.meta.title} />);
-        }) : <div>no posts</div>}
+        }) : <div>no posts</div>} */}
         </div>
     );
 }
--- src/components/tagsBox/TagsBox.tsx
+++ src/components/tagsBox/TagsBox.tsx
@@ -1,40 +1,41 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { trpc } from '../../utils/trpc';
-import { OutputPostsContext } from '../../pages';
+// import { OutputPostsContext } from '../../pages';
 const TagsBox: React.FC = () => {
-    const [selected, setSelected] = useState<string[]>([])
-    const [_, setOutputPosts] = useContext(OutputPostsContext)
-    const tags = trpc.tags.getTags.useQuery();    
-    const posts = trpc.postsByTag.getPostsByTag.useQuery(selected);
-    if (posts.isError) {
-        console.log('error happend', posts.error);
-    } else if (posts.data && posts.data.length > 0) {
-        console.log(posts.data)
-        setOutputPosts(posts.data);
-    }
-    useEffect(() => {
-        console.log(selected);
-    }, [selected])   
-    if (tags.isLoading) {
-        return (
-            <>
-            laoding
-            </>
-        );
-    }
-    // TODO change function setSelected to remove item if exist 
-    return (
-        <form className='flex gap-4 justify-center flex-wrap'>
-            {tags.data && tags.data.map((tag) => {
-                return (
-                    <>
-                    <label htmlFor={tag} className="w-auto px-4 cursor-pointer dark:bg-dark-primary-500 bg-gray-200 text-gray-600 font-mono font-semibold text-sm">{tag}</label>
-                    <input type="checkbox" id={tag} name={tag} value={tag} className="hidden " onChange={(e) => {setSelected(old => !old.includes(e.target.value) ? [...old, e.target.value] : old)}}/>
-                    </>
-                );
-            }
-            )}
-        </form>
-    );
+    return <div></div>
+    // const [selected, setSelected] = useState<string[]>([])
+    // const [_, setOutputPosts] = useContext(OutputPostsContext)
+    // const tags = trpc.tags.getTags.useQuery();    
+    // const posts = trpc.postsByTag.getPostsByTag.useQuery(selected);
+    // if (posts.isError) {
+    //     console.log('error happend', posts.error);
+    // } else if (posts.data && posts.data.length > 0) {
+    //     console.log(posts.data)
+    //     setOutputPosts(posts.data);
+    // }
+    // useEffect(() => {
+    //     console.log(selected);
+    // }, [selected])   
+    // if (tags.isLoading) {
+    //     return (
+    //         <>
+    //         laoding
+    //         </>
+    //     );
+    // }
+    // // TODO change function setSelected to remove item if exist 
+    // return (
+    //     <form className='flex gap-4 justify-center flex-wrap'>
+    //         {tags.data && tags.data.map((tag) => {
+    //             return (
+    //                 <>
+    //                 <label htmlFor={tag} className="w-auto px-4 cursor-pointer dark:bg-dark-primary-500 bg-gray-200 text-gray-600 font-mono font-semibold text-sm">{tag}</label>
+    //                 <input type="checkbox" id={tag} name={tag} value={tag} className="hidden " onChange={(e) => {setSelected(old => !old.includes(e.target.value) ? [...old, e.target.value] : old)}}/>
+    //                 </>
+    //             );
+    //         }
+    //         )}
+    //     </form>
+    // );
 }
 export default TagsBox;
