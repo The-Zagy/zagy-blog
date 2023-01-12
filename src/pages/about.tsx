87b10d3993080fb1eb1c1366568c7cfe72d5dd9c
sandstone991 <ahmed195138@feng.bu.edu.eg>
2023-01-12 19:17:10 +0200

    trigger deployment :) :) :) :) :)

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,17 +2,17 @@ import { NextPage } from "next";
 import Head from "next/head";
 const Home: NextPage = () => {
     return (
-    <>
-        <Head>
-            <title>About Us</title>
-            <meta name="description" content="Who Are We?" />
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <main className="container mx-auto flex min-h-screen flex-col items-center justify-center p-4">
-            <h1>If you want to write something open a pull request here <a href="https://github.com/The-Zagy/zagy-blog/tree/main/content/blog" className="dark:text-dark-secondary-500 text-dark-primary-700">Github</a></h1>
-        </main>
-    </>
+        <>
+            <Head>
+                <title>About Us</title>
+                <meta name="description" content="Who Are We?" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main className="container mx-auto flex min-h-screen flex-col items-center justify-center p-4">
+                <h1 className="text-2xl">Wanna write or edit a post? open a pull request <a href="https://github.com/The-Zagy/zagy-blog/tree/main/content/blog" className="dark:text-dark-secondary-500 text-dark-primary-700">here!</a></h1>
+            </main>
+        </>
     );
-  };
-  
-  export default Home;
+};
+
+export default Home;
