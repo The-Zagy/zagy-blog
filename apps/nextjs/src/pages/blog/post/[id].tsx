8f46fde4ef30b478383bc208930204effb260e62
sandstone991 <ahmed195138@feng.bu.edu.eg>
2023-03-02 16:08:58 +0200

    sowwy

--- apps/nextjs/src/pages/blog/post/[id].tsx
+++ apps/nextjs/src/pages/blog/post/[id].tsx
@@ -13,26 +13,6 @@ import Comments from "../../../components/comments/comments";
 import Link from "next/link";
 const GotToTopButton = dynamic(() => import('../../../components/goToTopButton/GoToTopButton'), { ssr: false });
 
-<div className="m-auto w-11/12 md:w-4/6 lg:w-3/6 text-xl flex flex-col py-20 gap-y-16 item-center justify-center" >
-    <PostHeader post={post} />
-    <main className="prose lg:prose-xl md:prose-md dark:prose-invert prose-pre:md:text-base break-words max-w-none" >
-        <Component components={MdxComponents} />
-    </main>
-    <div className="flex border-b-2 border-gray-50">
-        <a href={`${"https://github.com/The-Zagy/zagy-blog/edit/main"}/${post.githubPath}`}>Edit this on github</a>
-    </div>
-    <Comments />
-</div>
-        </>
-    );
-}
-export default PostPage;
-export const getStaticPaths: GetStaticPaths<{ slug: string }> = () => {
-    return {
-        paths: [],
-        fallback: 'blocking'
-    }
-}
 const getPostBySlug = async (slug: string) => {
     return await prisma.post.findFirst({
         where: {
@@ -183,7 +163,7 @@ const PostPage: NextPage<NonNullPostData> = (post) => {
             />
             <div className="m-auto w-11/12 md:w-4/6 lg:w-3/6 text-xl flex flex-col py-20 gap-y-16 item-center justify-center">
                 <PostHeader post={post} />
-                <main className="prose lg:prose-xl md:prose-md dark:prose-invert prose-pre:text-xl prose-pre:md:text-base break-words max-w-none">
+                <main className="prose lg:prose-xl md:prose-md dark:prose-invert prose-pre:md:text-base break-words max-w-none">
                     <Component components={MdxComponents} />
                 </main>
                 <div className="flex border-b-2 border-gray-50">
